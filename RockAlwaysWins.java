5db084b8ebc683b81759b2cbae48f3953bb0e03d
Caroline Choi <38522055+carolinechoi@users.noreply.github.com>
2020-01-13 14:24:31 -0800

    Update RockAlwaysWins.java

--- RockAlwaysWins.java
+++ RockAlwaysWins.java
@@ -7,30 +7,36 @@ public class RockAlwaysWins
 	private String compChoice;
 
 	public RockPaperScissors()
-  // set playChoice = compChoice = ""
+  	// set playChoice = compChoice = ""
 	{
-
+		playChoice = "";
+		compChoice = "";
 	}
 
 	public RockPaperScissors(String player) 
-  // what constructors do: set the instance vars. populate playChoice from the input, compchoice randomly
+  	// what constructors do: set the instance vars. populate playChoice from the input, compchoice randomly
 	{
-
+		playChoice = player;
+		double num = Math.random()*3;
+		if(num == 0)
+			compChoice = "S";
+		if(num == 1)
+			compChoice = "R";
+		if(num == 2)
+			compChoice = "P";
 	}
 
 	public String determineWinner()
-  // returns third line of output i.e. who wins and why
+  	// returns third line of output i.e. who wins and why
 	{
 		String winner="";
-
 		return winner;
 	}
 
 	public String toString()
 	{
-  // returns first two lines of output i.e. values of playChoice and compChoice
-		String output="";
-
+  	// returns first two lines of output i.e. values of playChoice and compChoice
+		String output="player had " + playChoice + "\ncomputer had " + compChoice;
 		return output;
 	}
 }
