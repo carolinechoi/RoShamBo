abb1dbd074b47a57eaa395b1b203978166a0d278
Caroline Choi <38522055+carolinechoi@users.noreply.github.com>
2020-01-13 14:37:47 -0800

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,6 +1,20 @@
+import java.util.scanner;
+
 public class Tester
 {
   public static void main(String[] args)
   {
+    RockPaperScissors rps = RockPaperScissors();
+    
+    String theirInput = "";
+    Scanner kb = Scanner(System.in);
+    while(theirInput != "n")
+    {
+      System.out.print("Rock-Paper-Scissors - pick your weapon[R,P,S]::");
+      theirInput = kb.next();
+      rps.RockPaperScissors(theirInput);
+      System.out.print("Do you want to play again?");
+      // theirInput = kb.next();
+    }
   }
 }
